bc25cfc2afb11f31662258331e8101c63e1917fd
kevinph6 <grandrobb0@gmail.com>
2024-07-02 21:44:30 +0100

    Helper.jsx

--- src/components/helper/Helper.jsx
+++ src/components/helper/Helper.jsx
@@ -9,7 +9,11 @@ export const faqs = [
   {
     question: "Why Lisana?",
     answer:
-      "The real question is: why not? $lisana is riding the waves of meme coins on the rising BASE Solana. Moon is programmed.",
+      "Innovative Fusion: Lisana combines digital art and gaming like never before, offering a unique and engaging experience.
+Secure Ownership: Using Solana blockchain technology, we ensure your digital assets are secure and truly yours.
+Fair Rewards: Our platform guarantees fair compensation and rewards for both artists and gamers through transparent transactions.
+Vibrant Community: Join a thriving community of creators and players, and participate in exclusive events and tournaments.
+Cutting-Edge Technology: Leveraging the speed and efficiency of the Solana blockchain, Lisana provides a seamless and enjoyable user experience.",
   },
   {
     question: "What is the contract address?",
