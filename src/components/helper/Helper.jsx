1274a0143d8bd5acdaf7637413a09441c2846244
kevinph6 <grandrobb0@gmail.com>
2024-07-03 00:34:31 +0100

    Helper.jsx

--- src/components/helper/Helper.jsx
+++ src/components/helper/Helper.jsx
@@ -5,25 +5,40 @@ export const mediaicons = [
   { icon: <TwitterIcon />, link: "https://x.com" },
   { icon: <InstagramIcon />, link: "https://www.instagram.com" },
 ];
+
 export const faqs = [
   {
-    question: "Why Lisankfkfkkfa?",
-    answer:
-      "test.",
+    question: "Why Lisana?",
+    answer: `
+      Innovative Fusion: Lisana combines digital art and gaming like never before, offering a unique and engaging experience.
+      Secure Ownership: Using Solana blockchain technology, we ensure your digital assets are secure and truly yours.
+      Fair Rewards: Our platform guarantees fair compensation and rewards for both artists and gamers through transparent transactions.
+      Vibrant Community: Join a thriving community of creators and players, and participate in exclusive events and tournaments.
+      Cutting-Edge Technology: Leveraging the speed and efficiency of the Solana blockchain, Lisana provides a seamless and enjoyable user experience.
+    `,
   },
   {
-    question: "What is the contract address?",
-    answer:
-      "The real question is: why not? $lisana is riding the waves of meme coins on the rising BASE Solana. Moon is programmed.",
+    question: "Where can I buy Lisana?",
+    answer: `
+      $LISANA Can Be Acquired During Our Presale Event, Which Will Be Announced On Our Official Website And Social Media Platforms. Following The Presale, $LISANA Will Be Available For Trading On Various Cryptocurrency Exchanges, Providing Investors With Ample Opportunities To Purchase And Engage With This Exciting New Asset.
+    `,
   },
   {
-    question: "Where can we buy?",
-    answer:
-      "The real question is: why not? $lisana is riding the waves of meme coins on the rising BASE Solana. Moon is programmed.",
+    question: "Can I buy $LISANA on presale?",
+    answer: `
+      Yes, that's the goal. You can easily send SOL to 9JyJcgmcGqsrqD2SfV58yovHgCqykSUncKHdtFxhwiGS.
+    `,
   },
   {
     question: "How can I contact the team?",
-    answer:
-      "The real question is: why not? $lisana is riding the waves of meme coins on the rising BASE Solana. Moon is programmed.",
+    answer: `
+      For Any Inquiries, Assistance, Or Feedback Regarding $Lisana, You Can Easily Reach Out To The Lisana Team Via Our Social Media Channels. Our Dedicated Support Team Is Here To Address Your Questions And Ensure A Seamless Experience As You Explore The World Of $Lisana.
+    `,
+  },
+  {
+    question: "Is $Lisana secure?",
+    answer: `
+      Security Is Of Utmost Importance To Us At $Lisana. We Employ Robust Measures Such As Regular Security Audits, Encryption Protocols, And Compliance With Regulatory Standards To Safeguard Our Platform And Users' Investments. Our Commitment To Transparency And Adherence To Best Practices Ensure That $Lisana Provides A Secure Environment For Investors To Participate In The Cryptocurrency Market Confidently.
+    `,
   },
 ];
