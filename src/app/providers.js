47d2516b4de61291bbe0a1d7c5fbb84841c022ea
wakechromium <hafiz1tayab@gmail.com>
2024-07-22 21:36:47 +0500

    mainnet

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,7 +1,5 @@
 "use client";
 
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { clusterApiUrl } from "@solana/web3.js";
 import {
   PhantomWalletAdapter,
   TrustWalletAdapter,
@@ -14,13 +12,13 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 
-const network = WalletAdapterNetwork.Devnet;
-const endpoint = clusterApiUrl(network);
+const rpcUrl =
+  "https://mainnet.helius-rpc.com/?api-key=a6960cc7-9546-44ef-b51c-83c7026d34e3";
 const wallets = [new PhantomWalletAdapter(), new TrustWalletAdapter()];
 
 export default function Providers({ children }) {
   return (
-    <ConnectionProvider endpoint={endpoint}>
+    <ConnectionProvider endpoint={rpcUrl}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
